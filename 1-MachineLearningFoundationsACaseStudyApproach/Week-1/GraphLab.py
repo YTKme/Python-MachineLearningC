c1b58d58ca02be9cb03b5e7f22898356b6b6d064
Yan Kuang <YTKme@Outlook.com>
2019-04-13 00:38:42 -0700

    Update Week-1 notebook

--- 1-MachineLearningFoundationsACaseStudyApproach/Week-1/GraphLab.py
+++ 1-MachineLearningFoundationsACaseStudyApproach/Week-1/GraphLab.py
@@ -7,6 +7,6 @@ graphlab.product_key.get_product_key()
 sf = graphlab.SFrame('people-example.csv')
 print sf
 
-graphlab.canvas.set_target('ipynb')
+#graphlab.canvas.set_target('ipynb')
 
 sf['age'].show(view='Categorical')
